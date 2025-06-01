ab3367cc82d299bcd4e250db48bdd46081d34374
Armana Tursymbayeva <armana111404@gmail.com>
2025-06-01 20:55:06 +0500

    service for saving users

---
+++ src/main/java/com/example/taskapi/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.taskapi.service;
+
+import com.example.taskapi.model.User;
+import com.example.taskapi.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email).orElse(null);
+    }
+}
