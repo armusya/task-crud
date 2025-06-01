cc824c6d1ac7047c6eab24fb46a203db25b7f8ec
Armana Tursymbayeva <armana111404@gmail.com>
2025-06-01 20:54:12 +0500

    add responsible for loading user-specific data

---
+++ src/main/java/com/example/taskapi/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.taskapi.service;
+
+import com.example.taskapi.model.User;
+import com.example.taskapi.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+
+}
